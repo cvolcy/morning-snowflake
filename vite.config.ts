220c082499b70a5c2283d045accdf7541c8a01e5
Clervens Volcy <clervens.volcy@gmail.com>
2024-12-01 12:47:50 -0500

    fix kaspa import in config

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/libs/kaspa-rpc/kaspa.js'),
+      name: 'kaspa-rpc',
+      fileName: 'kaspa.js'
+    }
+  }
 })
