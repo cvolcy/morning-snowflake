dadd4ff903a15fcc75aed5cc885052aabd75a8de
Clervens Volcy <clervens.volcy@gmail.com>
2024-12-01 23:57:38 -0500

    update router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import KastaView from '../views/KastaView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: import('../views/KastaView.vue'),
+      component: KastaView,
     },
     {
       path: '/about',
